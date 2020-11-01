67ba246da6624eeb7e8401f0297436b7c39b4a69
Hamid <hamidef@umich.edu>
2020-11-01 16:32:46 -0500

    site update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,12 +2,12 @@ var store = [{
         "title": "c-Optimal Designs for High-dimensional Linear Models",
         "excerpt":"Simulation experiments for our paper on c-optimal designs in high-dimensional linear models are available in this Jupyter notebook. Check out the pre-print on Arxiv: https://arxiv.org/abs/2010.12580.  ","categories": [],
         "tags": [],
-        "url": "/portfolio/c_optimality/",
-        "teaser": "/assets/images/c_optimal/shepp_lap_filter.png"
+        "url": "https://ehamid.github.io/portfolio/c_optimality/",
+        "teaser": "https://ehamid.github.io/assets/images/c_optimal/shepp_lap_filter.png"
       },{
         "title": "Inference for High-dimensional Single-Index Models",
         "excerpt":"Preprint on Arxiv: https://arxiv.org/abs/1909.03540 In this project we developed methods for constructing confidence intervals and hypothesis tests for the linear component of high-dimensional single-index models. Single-index models generalize linear models by allowing the reponse $ y $ to depend on a non-linear function of a linear combination of the predictors:...","categories": [],
         "tags": [],
-        "url": "/portfolio/hd_sim_inference/",
-        "teaser": "/assets/images/hd_sim/histogram.png"
+        "url": "https://ehamid.github.io/portfolio/hd_sim_inference/",
+        "teaser": "https://ehamid.github.io/assets/images/hd_sim/histogram.png"
       },]
