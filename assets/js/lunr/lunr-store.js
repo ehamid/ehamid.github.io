1dacc472f524615ebbfa119bc0f73119bb0920e1
Hamid <hamidef@umich.edu>
2021-01-28 21:41:44 -0500

    add new project

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,18 +2,18 @@ var store = [{
         "title": "c-Optimal Designs for High-dimensional Linear Models",
         "excerpt":"Simulation experiments for our paper on c-optimal designs in high-dimensional linear models are available in this Jupyter notebook. Check out the pre-print on Arxiv: https://arxiv.org/abs/2010.12580.  ","categories": [],
         "tags": [],
-        "url": "https://ehamid.github.io/portfolio/c_optimality/",
-        "teaser": "https://ehamid.github.io/assets/images/c_optimal/shepp_lap_filter.png"
+        "url": "http://localhost:4000/portfolio/c_optimality/",
+        "teaser": "http://localhost:4000/assets/images/c_optimal/shepp_lap_filter.png"
       },{
         "title": "Inference for High-dimensional Single-Index Models",
         "excerpt":"Preprint on Arxiv: https://arxiv.org/abs/1909.03540 In this project we developed methods for constructing confidence intervals and hypothesis tests for the linear component of high-dimensional single-index models. Single-index models generalize linear models by allowing the reponse $ y $ to depend on a non-linear function of a linear combination of the predictors:...","categories": [],
         "tags": [],
-        "url": "https://ehamid.github.io/portfolio/hd_sim_inference/",
-        "teaser": "https://ehamid.github.io/assets/images/hd_sim/histogram.png"
+        "url": "http://localhost:4000/portfolio/hd_sim_inference/",
+        "teaser": "http://localhost:4000/assets/images/hd_sim/histogram.png"
       },{
         "title": "Individually Fair Rankings",
         "excerpt":"This is joint work with Amanda Bower, Mikhail Yurochkin and Yuekai Sun and was accepted to ICLR 2021 for a poster presentation. Abstract: We develop an algorithm to train individually fair learning-to-rank (LTR) models. The proposed approach ensures items from minority groups appear alongside similar items from majority groups. This...","categories": [],
         "tags": [],
-        "url": "https://ehamid.github.io/portfolio/ind_fair_rank/",
-        "teaser": "https://ehamid.github.io/assets/images/ind_fair_rank/ndcg.png"
+        "url": "http://localhost:4000/portfolio/ind_fair_rank/",
+        "teaser": "http://localhost:4000/assets/images/ind_fair_rank/ndcg.png"
       },]
